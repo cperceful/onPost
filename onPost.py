09ce21f14b80528d966be6054dfad36d1bf94d6b
cperceful <cpercef1@gmail.com>
2016-12-27 20:44:20 -0600

    basic functionality in place. Output file needs a little formatting to make it prettier

--- onPost.py
+++ onPost.py
@@ -3,15 +3,25 @@
 import xlrd; #module for reading from excel files
 import xlwt; #module for writing to excel files
 
-workbook = xlrd.open_workbook('SGWS UPS AND DOWNS SEPT 2016.xlsx'); #opens the file and stores as variable to manipulate
+
+workbook = xlrd.open_workbook('target.xlsx'); #opens the file and stores as variable to manipulate
 worksheet = workbook.sheet_by_index(0);
 
 #create a new, empty file
 newWorkbook = xlwt.Workbook();
 newSheet = newWorkbook.add_sheet('Sheet 1')
-newWorkbook.save('testFile.ods');
+
+
 
 #test extraction of data
+counter = 0;
 for i in range(worksheet.nrows):
     if worksheet.cell(i, 10).value > worksheet.cell(i, 8).value:
-        print(worksheet.cell(i, 1));
+        #print(worksheet.cell(i, 1)); #test printing of relevant data
+        newSheet.write(counter, 0, worksheet.cell(i, 1).value)
+        counter += 1;
+
+newWorkbook.save('onPost.ods')
+print('Program complete. File saved as onPost.ods');
+
+#It works. Program needs to alter formatting of spreadsheet
