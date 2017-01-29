ee60432258960b639573a580635c9f032cda81bb
cperceful <cpercef1@gmail.com>
2017-01-28 23:38:32 -0600

    first attempt at document with three month pricing data

--- onPost.py
+++ onPost.py
@@ -3,6 +3,7 @@
 import xlrd; #module for reading from excel files
 import xlwt; #module for writing to excel files
 
+#column 8 is Jan Price, 10 is Feb Price, 12 is Mar price
 
 def writeOn(oldSheet, newSheet):
     counter = 1; #start at 1, first row will contain column names
@@ -10,24 +11,38 @@ def writeOn(oldSheet, newSheet):
 
         if (oldSheet.cell(i, 2).value == '1L'):
             continue; #skip 1L sizes
-        elif (oldSheet.cell(i, 12).value > oldSheet.cell(i, 10).value): #used to be >=, doesn't matter for end of month, only regular can buy list
+        elif (oldSheet.cell(i, 8).value < oldSheet.cell(i, 10).value and oldSheet.cell(i, 8).value == oldSheet.cell(i, 12).value): #used to be >=, doesn't matter for end of month, only regular can buy list
             #print(worksheet.cell(i, 1)); #test printing of relevant data
             newSheet.write(counter, 0, str(oldSheet.cell(i, 0).value) + " " + str(oldSheet.cell(i, 2).value)); #writes product name and size
-            newSheet.write(counter, 1, str(oldSheet.cell(i, 10).value)); #writes January price
-            newSheet.write(counter, 2, str(oldSheet.cell(i, 12).value)); #writes February price
+            newSheet.write(counter, 1, str(oldSheet.cell(i, 8).value)); #writes January price
+            newSheet.write(counter, 2, str(oldSheet.cell(i, 10).value)); #writes February price
             counter += 1;
     newSheet.write(counter + 1, 0, 'Vampire Pilot Studios, Enterprise Division');
     newSheet.write(counter + 2, 0, "Hi Rob, didn't you say you'd pay me for this?");
+
+def writeTwoMonths(oldSheet, newSheet):
+    counter = 1;
+    for i in range(oldSheet.nrows):
+        if (oldSheet.cell(i, 2).value == '1L'):
+            continue; #skip 1L sizes
+        elif (oldSheet.cell(i, 8).value < oldSheet.cell(i, 10).value and oldSheet.cell(i, 10).value == oldSheet.cell(i, 12).value):
+            newSheet.write(counter, 0, str(oldSheet.cell(i, 0).value) + " " + str(oldSheet.cell(i, 2).value)); #product name and size
+            newSheet.write(counter, 1, str(oldSheet.cell(i, 8).value)); #write Jan price
+            newSheet.write(counter, 2, str(oldSheet.cell(i, 10).value)); #write Feb price
+            newSheet.write(counter, 3, str(oldSheet.cell(i, 12).value)); #write Mar price
+            counter += 1;
+
 
 def writeMid(oldSheet, newSheet):
     counter = 1; #start at 1, first row will contain column names
     for i in range(oldSheet.nrows):
         if (oldSheet.cell(i, 2).value == '1L'):
             continue;
-        elif (oldSheet.cell(i, 10).value < oldSheet.cell(i, 12).value and (oldSheet.cell(i, 8).value < oldSheet.cell(i, 10).value)):
+        elif (oldSheet.cell(i, 8).value < oldSheet.cell(i, 10).value and (oldSheet.cell(i, 12).value < oldSheet.cell(i, 8).value)):
             newSheet.write(counter, 0, str(oldSheet.cell(i, 0).value) + " " + str(oldSheet.cell(i, 2).value)); #writes product name
-            newSheet.write(counter, 1, str(oldSheet.cell(i, 10).value)); #writes January price
-            newSheet.write(counter, 2, str(oldSheet.cell(i, 12).value)); #writes February price
+            newSheet.write(counter, 1, str(oldSheet.cell(i, 8).value)); #writes January price
+            newSheet.write(counter, 2, str(oldSheet.cell(i, 10).value)); #writes February price
+            newSheet.write(counter, 3, str(oldSheet.cell(i, 12).value));
             counter += 1;
 
 def writeFlat(oldSheet, newSheet):
@@ -35,22 +50,39 @@ def writeFlat(oldSheet, newSheet):
     for i in range(oldSheet.nrows):
         if (oldSheet.cell(i, 2).value == '1L'):
             continue;
-        elif (oldSheet.cell(i, 10).value == oldSheet.cell(i, 12).value):
+        elif (oldSheet.cell(i, 8).value == oldSheet.cell(i, 10).value and oldSheet.cell(i, 10).value == oldSheet.cell(i, 12).value):
             newSheet.write(counter, 0, str(oldSheet.cell(i, 0).value) + " " + str(oldSheet.cell(i, 2).value)); #writes product name
-            newSheet.write(counter, 1, str(oldSheet.cell(i, 10).value));
-            newSheet.write(counter, 2, str(oldSheet.cell(i, 12).value));
+            newSheet.write(counter, 1, str(oldSheet.cell(i, 8).value)); #write Jan price
+            newSheet.write(counter, 2, str(oldSheet.cell(i, 10).value)); #write Feb price
+            newSheet.write(counter, 3, str(oldSheet.cell(i, 12).value)); #write March
             counter += 1;
 
 
-def setColumns(sheet):
+# def setColumns(sheet):
+#     sheet.col(0).width = 15000;
+#     sheet.col(1).width = 4000;
+#     sheet.col(2).width = 4000;
+#     sheet.write(0, 0, "Product");
+#     sheet.write(0, 1, "January Price");
+#     sheet.write(0, 2, "February Price");
+
+def oneMonthColumns(sheet):
     sheet.col(0).width = 15000;
     sheet.col(1).width = 4000;
     sheet.col(2).width = 4000;
-    sheet.write(0, 0, "Product");
-    sheet.write(0, 1, "January Price");
-    sheet.write(0, 2, "February Price");
+    sheet.write(0, 0, 'One Month Posts');
+    sheet.write(0, 1, 'January Price');
+    sheet.write(0, 2, 'February Price');
 
-
+def twoMonthColumns(sheet):
+    sheet.col(0).width = 15000;
+    sheet.col(1).width = 4000;
+    sheet.col(2).width = 4000;
+    sheet.col(3).width = 4000;
+    sheet.write(0, 0, 'Two Month Posts');
+    sheet.write(0, 1, 'January Price');
+    sheet.write(0, 2, 'February Price');
+    sheet.write(0, 3, 'March Price');
 
 
 workbook = xlrd.open_workbook('target.xls'); #opens the file and stores as variable to manipulate
@@ -58,15 +90,18 @@ worksheet = workbook.sheet_by_index(0);
 
 #create a new, empty file
 newWorkbook = xlwt.Workbook();
-onSheet = newWorkbook.add_sheet('On Post');
+oneMonthPost = newWorkbook.add_sheet('On Post One Month');
+twoMonthPost = newWorkbook.add_sheet('On Post Two Months');
 midSheet = newWorkbook.add_sheet('Mid Post');
 flatSheet = newWorkbook.add_sheet('Flat post');
 
-setColumns(onSheet);
-setColumns(midSheet);
-setColumns(flatSheet);
+oneMonthColumns(oneMonthPost);
+twoMonthColumns(twoMonthPost);
+twoMonthColumns(midSheet);
+twoMonthColumns(flatSheet);
 
-writeOn(worksheet, onSheet);
+writeOn(worksheet, oneMonthPost);
+writeTwoMonths(worksheet, twoMonthPost);
 writeMid(worksheet, midSheet);
 writeFlat(worksheet, flatSheet);
 
